ae634073d2adbba751dc361478d90c3bea9b06e9
Estebanp42 <esteban.p@knights.ucf.edu>
2019-09-21 16:15:48 -0400

    Second Assignment Environment

--- OOP/src/Fig2_1.java
+++
@@ -1,8 +0,0 @@
-
-public class Fig2_1 {
-	//main method begins execution of the Java application
-		public static void main(String[] args) {
-			System.out.println("Welcome \nto\n\"Java Programming!\"");
-		} //end method main
-	} //end class Fig2_1
-
